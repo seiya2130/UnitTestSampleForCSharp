ec7ad6437387ea778887e4b5e5e7c17fe72b5736
Seiya_Satoh <satohseiya0312@gmail.com>
2021-08-26 10:38:24 +0900

    プロジェクト ファイルを追加します。

---
+++ UnitTestSampleForCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestSampleForCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
