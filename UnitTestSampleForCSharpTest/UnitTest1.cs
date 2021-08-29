ac41314044628d2b62ab981d62c6456924a6ac4b
Seiya_Satoh <satohseiya0312@gmail.com>
2021-08-29 17:15:56 +0900

    [update]演算テストの中身を実装

--- UnitTestSampleForCSharpTest/UnitTest1.cs
+++ UnitTestSampleForCSharpTest/UnitTest1.cs
@@ -1,34 +1,50 @@
 using NUnit.Framework;
 
 namespace UnitTestSampleForCSharpTest
-{
+{   
+    [TestFixture]
     public class Tests
     {
+        int a;
+        int b;
+
         [SetUp]
         public void Setup()
         {
+            this.a = 10;
+            this.b = 5;
         }
 
         [Test]
-        public void AddTest()
+        [TestCase(10, 5)]
+        public void AddTest(int a, int b)
         {
-            Assert.Pass();
+            Assert.AreEqual(a + b, 15);
+            Assert.AreNotEqual(a + b, 16);
         }
 
         [Test]
-        public void SubtractTest()
+        [TestCase(10, 5)]
+        public void SubtractTest(int a, int b)
         {
-            Assert.Pass();
+            Assert.AreEqual(a - b, 5);
+            Assert.AreNotEqual(a - b, 6);
         }
+
         [Test]
-        public void MultiplyTest()
+        [TestCase(10, 5)]
+        public void MultiplyTest(int a, int b)
         {
-            Assert.Pass();
+            Assert.AreEqual(a * b, 50);
+            Assert.AreNotEqual(a * b, 51);
         }
+
         [Test]
-        public void DivideTest()
+        [TestCase(10, 5)]
+        public void DivideTest(int a, int b)
         {
-            Assert.Pass();
+            Assert.AreEqual(a / b, 2);
+            Assert.AreNotEqual(a / b, 3);
         }
     }
 }
