8123b3b971adc8f4b8de45db7dee2f62f9f79bb5
Seiya_Satoh <satohseiya0312@gmail.com>
2021-08-29 17:40:25 +0900

    [add]計算クラス追加

---
+++ UnitTestSampleForCSharp/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestSampleForCSharp
+{
+    public static class Calculator
+    {
+        public static void CheckNull(int? a, int? b)
+        {
+            if (a == null || b == null)
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        public static int Add(int a, int b)
+        {
+            CheckNull(a, b);
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            CheckNull(a, b);
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            CheckNull(a, b);
+            return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            CheckNull(a, b);
+            return a / b;
+        }
+    }
+}
