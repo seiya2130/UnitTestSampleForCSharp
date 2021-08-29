c02c6acc29387f563d48db9666fa77ac752e2e2a
Seiya_Satoh <satohseiya0312@gmail.com>
2021-08-29 18:27:34 +0900

    [add]Moqの導入、インターフェースのテスト追加

---
+++ UnitTestSampleForCSharpTest/InterfaceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using UnitTestSampleForCSharp;
+
+namespace UnitTestSampleForCSharpTest
+{
+    [TestFixture]
+    public class InterfaceTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            var mock = new Mock<ISampleInterface>();
+            mock.Setup(x => x.Sample()).Returns("Hello Mock");
+
+            Assert.AreEqual(mock.Object.Sample(), "Hello Mock");
+        }
+    }
+}
