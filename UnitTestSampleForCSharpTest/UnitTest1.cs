80b31ea9e288125714351a6c36f6a5bea23bde00
Seiya_Satoh <satohseiya0312@gmail.com>
2021-08-29 17:58:31 +0900

    [update]Calculatorのメソッドテストに修正

--- UnitTestSampleForCSharpTest/UnitTest1.cs
+++ UnitTestSampleForCSharpTest/UnitTest1.cs
@@ -11,37 +11,6 @@ namespace UnitTestSampleForCSharpTest
         {
         }
 
-        [Test]
-        [TestCase(10, 5)]
-        public void AddTest(int a, int b)
-        {
-            Assert.AreEqual(a + b, 15);
-            Assert.AreNotEqual(a + b, 16);
-        }
-
-        [Test]
-        [TestCase(10, 5)]
-        public void SubtractTest(int a, int b)
-        {
-            Assert.AreEqual(a - b, 5);
-            Assert.AreNotEqual(a - b, 6);
-        }
-
-        [Test]
-        [TestCase(10, 5)]
-        public void MultiplyTest(int a, int b)
-        {
-            Assert.AreEqual(a * b, 50);
-            Assert.AreNotEqual(a * b, 51);
-        }
-
-        [Test]
-        [TestCase(10, 5)]
-        public void DivideTest(int a, int b)
-        {
-            Assert.AreEqual(a / b, 2);
-            Assert.AreNotEqual(a / b, 3);
-        }
 
         [Test]
         [TestCase(null, 5)]
@@ -49,11 +18,49 @@ namespace UnitTestSampleForCSharpTest
         [TestCase(null, null)]
         public void NullExceptionTest(int? a, int? b)
         {
-            if(a == null || b == null)
-            {
-                Assert.Throws<NullReferenceException>(
-                () => { throw new NullReferenceException(); });
-            }
+            Assert.Throws<NullReferenceException>(
+            () => { UnitTestSampleForCSharp.Calculator.CheckNull(a, b); });
+
+        }
+
+        [Test]
+        [TestCase(10, 5)]
+        public void NotNullExceptionTest(int? a, int? b)
+        {
+            Assert.DoesNotThrow(
+            () => { UnitTestSampleForCSharp.Calculator.CheckNull(a, b); });
+        }
+
+        [Test]
+        [TestCase(10, 5)]
+        public void AddTest(int a, int b)
+        {
+            Assert.AreEqual(UnitTestSampleForCSharp.Calculator.Add(a, b), 15);
+            Assert.AreNotEqual(UnitTestSampleForCSharp.Calculator.Add(a, b), 16);
+        }
+
+        [Test]
+        [TestCase(10, 5)]
+        public void SubtractTest(int a, int b)
+        {
+            Assert.AreEqual(UnitTestSampleForCSharp.Calculator.Subtract(a, b), 5);
+            Assert.AreNotEqual(UnitTestSampleForCSharp.Calculator.Subtract(a, b), 6);
+        }
+
+        [Test]
+        [TestCase(10, 5)]
+        public void MultiplyTest(int a, int b)
+        {
+            Assert.AreEqual(UnitTestSampleForCSharp.Calculator.Multiply(a, b), 50);
+            Assert.AreNotEqual(UnitTestSampleForCSharp.Calculator.Multiply(a, b), 51);
+        }
+
+        [Test]
+        [TestCase(10, 5)]
+        public void DivideTest(int a, int b)
+        {
+            Assert.AreEqual(UnitTestSampleForCSharp.Calculator.Divide(a, b), 2);
+            Assert.AreNotEqual(UnitTestSampleForCSharp.Calculator.Divide(a, b), 3);
         }
     }
 }
