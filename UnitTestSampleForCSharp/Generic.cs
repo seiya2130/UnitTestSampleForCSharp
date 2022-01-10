c2db34a80a5d2d92c682b31c5c231f7ed4db79c4
Seiya_Satoh <satohseiya0312@gmail.com>
2022-01-10 18:33:38 +0900

    [update]ジェネリック型プロパティ追加

--- UnitTestSampleForCSharp/Generic.cs
+++ UnitTestSampleForCSharp/Generic.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace UnitTestSampleForCSharp
 {
-    public class Generic
+    public class Generic<T>
     {
+        public T Prop1;
         public static bool IsValueWithinRange<Type>(Type value, Type from, Type to) where Type : IComparable
         {
             return value.CompareTo(from) >= 0 && to.CompareTo(value) >= 0;
